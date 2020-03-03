b18fdae7b62f244148af974fde487f0320cc4674
rupambumba <bybumba@gmail.com>
2020-03-03 10:12:49 +0530

    set up server.js

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const path = require('path');
 
 const app = express();
 
+
+
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+
 
 // An api endpoint that returns a short list of items
 app.get('/api/getList', (req,res) => {
@@ -13,12 +18,15 @@ app.get('/api/getList', (req,res) => {
     console.log('Sent list of items');
 });
 
+
+
+
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
 
-console.log('App is listening on port ' + port);
+module.exports = app;
+
+
